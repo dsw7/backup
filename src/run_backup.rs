18bf62d289cfbb8f7ef770204a8a804677392029
dsw7 <dds3443@hotmail.com>
2026-07-25 01:42:46 -0700

    Add helper for appending slash to source

--- src/run_backup.rs
+++ src/run_backup.rs
@@ -1,6 +1,18 @@
 use std::process::Command;
 
-pub fn run_backup(src: &String, dst: &String) {
+use crate::configs::Config;
+
+fn append_slash_to_source(source: &String) -> String {
+    if source.ends_with('/') {
+        String::from(source)
+    } else {
+        String::from(format!("{source}/"))
+    }
+}
+
+fn run_rsync_hot_storage_dry_run(configs: &Config) {
+    let src = append_slash_to_source(&configs.source);
+    let dst = String::from("dsw@10.0.0.115:/tmp/bar");
     let status = Command::new("rsync")
         .arg("-av")
         .arg("--dry-run")
@@ -11,4 +23,8 @@ pub fn run_backup(src: &String, dst: &String) {
         .expect("Command failed to start. There is no way to proceed");
 
     println!("Process exited with status: {}", status);
+}
+
+pub fn run_backup(configs: &Config) {
+    run_rsync_hot_storage_dry_run(&configs);
 }
