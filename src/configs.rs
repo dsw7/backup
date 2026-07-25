cbb585c64334fb1cfc21f232015057a0213b08a9
dsw7 <dds3443@hotmail.com>
2026-07-24 23:18:01 -0700

    Tweak configs pipeline

--- src/configs.rs
+++ src/configs.rs
@@ -42,20 +42,30 @@ fn get_home_dir() -> Result<PathBuf, io::Error> {
     }
 }
 
-fn get_config_file_path() -> Result<PathBuf, io::Error> {
-    let home_dir = get_home_dir()?;
-
+fn get_config_file_path(home_dir: &PathBuf) -> Result<PathBuf, io::Error> {
     let mut config_file_path = PathBuf::from(home_dir);
     config_file_path.push(".backup/config.toml");
 
     Ok(config_file_path)
 }
 
+fn read_config_file_to_string(config_file_path: &PathBuf) -> Result<String, io::Error> {
+    match fs::read_to_string(config_file_path) {
+        Ok(contents) => Ok(contents),
+        Err(e) => {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("Failed to read '{}': {e}", config_file_path.display()),
+            ));
+        }
+    }
+}
+
 pub fn load_configs() -> Result<Config, io::Error> {
-    let config_file = get_config_file_path()?;
-    let contents = fs::read_to_string(config_file)?;
+    let home_dir = get_home_dir()?;
+    let config_file = get_config_file_path(&home_dir)?;
+    let contents = read_config_file_to_string(&config_file)?;
 
     let config: Config = toml::from_str(&contents).expect("Failed to parse TOML content");
-
     Ok(config)
 }
