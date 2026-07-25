fc23264e1410c1ac087606997140b3115ddbf66f
dsw7 <dds3443@hotmail.com>
2026-07-25 02:11:55 -0700

    Add destination selector

--- src/run_backup.rs
+++ src/run_backup.rs
@@ -23,14 +23,23 @@ fn format_destination(user: &String, host: &String, destination: &String) -> Str
     format!("{user}@{host}:{dst}")
 }
 
-fn run_rsync_hot_storage_dry_run(configs: &Config) {
-    let src = append_slash_to_source(&configs.source);
-    let dst = format_destination(
-        &configs.storage.hot.user,
-        &configs.storage.hot.host,
-        &configs.storage.hot.destination,
-    );
+fn select_destination(sync_to_hot: bool, configs: &Config) -> String {
+    if sync_to_hot {
+        format_destination(
+            &configs.storage.hot.user,
+            &configs.storage.hot.host,
+            &configs.storage.hot.destination,
+        )
+    } else {
+        format_destination(
+            &configs.storage.cold.user,
+            &configs.storage.cold.host,
+            &configs.storage.cold.destination,
+        )
+    }
+}
 
+fn run_rsync_dry_run(src: &String, dst: &String) {
     let status = Command::new("rsync")
         .arg("-av")
         .arg("--dry-run")
@@ -44,5 +53,9 @@ fn run_rsync_hot_storage_dry_run(configs: &Config) {
 }
 
 pub fn run_backup(configs: &Config) {
-    run_rsync_hot_storage_dry_run(&configs);
+    let sync_to_hot = true;
+
+    let src = append_slash_to_source(&configs.source);
+    let dst = select_destination(sync_to_hot, &configs);
+    run_rsync_dry_run(&src, &dst);
 }
