1d176c5b3c04a9631e887574ccc2e1f7f6709d9d
dsw7 <dds3443@hotmail.com>
2026-07-27 04:24:55 -0700

    Return io::Error

--- src/data_directory.rs
+++ src/data_directory.rs
@@ -1,27 +1,27 @@
-use crate::errors::BackupError;
-
 use std::env;
+use std::io::{self, Error, ErrorKind};
 use std::path::PathBuf;
 
-fn get_home_dir() -> Result<PathBuf, BackupError> {
+fn get_home_dir() -> io::Result<PathBuf> {
     match env::home_dir() {
         Some(path) => Ok(path),
-        None => Err(BackupError::Other(String::from(
+        None => Err(Error::new(
+            ErrorKind::NotFound,
             "Couldn't get home directory",
-        ))),
+        )),
     }
 }
 
-pub fn get_data_dir() -> Result<PathBuf, BackupError> {
+pub fn get_data_dir() -> io::Result<PathBuf> {
     let home_dir = get_home_dir()?;
     let program_dir = PathBuf::from(home_dir).join(".backup");
 
     if program_dir.exists() {
         Ok(program_dir)
     } else {
-        Err(BackupError::Other(format!(
-            "Directory '{}' does not exist",
-            program_dir.display()
-        )))
+        Err(Error::new(
+            ErrorKind::NotFound,
+            "Program data directory does not exist",
+        ))
     }
 }
