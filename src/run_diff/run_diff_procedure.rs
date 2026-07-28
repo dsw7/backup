ca89081960d2da6de65031de762614116a70ff14
dsw7 <dds3443@hotmail.com>
2026-07-28 00:18:46 -0700

    Print success and fail cases

--- src/run_diff/run_diff_procedure.rs
+++ src/run_diff/run_diff_procedure.rs
@@ -1,10 +1,30 @@
 use crate::configs::Configs;
 use crate::errors::BackupError;
 
-use super::subprocesses::get_disk_usages;
+use super::subprocesses::{Usage, get_disk_usages};
+
+fn display_usages(usages: Vec<Usage>) {
+    for usage in usages {
+        println!("{} {}", usage.host, usage.stdout);
+    }
+}
+
+fn display_failed_usages(failed_usages: Vec<Usage>) {
+    for usage in failed_usages {
+        println!("{} {}", usage.host, usage.stderr);
+    }
+}
+
+fn partition_usages(usages: &mut Vec<Usage>) -> Vec<Usage> {
+    usages.extract_if(.., |usage| !usage.failed).collect()
+}
 
 pub fn run_diff_procedure(configs: &Configs) -> Result<(), BackupError> {
-    let usages = get_disk_usages(&configs)?;
+    let mut usages = get_disk_usages(&configs)?;
+    let failed_usages = partition_usages(&mut usages);
+
+    display_usages(usages);
+    display_failed_usages(failed_usages);
 
     Ok(())
 }
