2b627110e7a4004812d232b877f37b5181ac3efb
dsw7 <dds3443@hotmail.com>
2026-07-27 22:13:14 -0700

    Get local usage

--- src/run_diff/run_diff_procedure.rs
+++ src/run_diff/run_diff_procedure.rs
@@ -1,6 +1,21 @@
 use crate::configs::Configs;
 use crate::errors::BackupError;
 
+use std::io;
+use std::process::Command;
+
+fn get_disk_usages(configs: &Configs) -> io::Result<()> {
+    let mut proc_local = Command::new("du")
+        .args(["--summarize", "--bytes", &configs.source])
+        .spawn()?;
+
+    let status = proc_local.wait()?;
+    println!("{status}");
+    Ok(())
+}
+
 pub fn run_diff_procedure(configs: &Configs) -> Result<(), BackupError> {
+    let usages = get_disk_usages(&configs)?;
+
     Ok(())
 }
