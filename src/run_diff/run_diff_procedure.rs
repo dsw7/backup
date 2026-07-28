f79034ce5122d8a07c3583bb47b8248e7ea3e917
dsw7 <dds3443@hotmail.com>
2026-07-27 22:36:43 -0700

    Unpack stdout and stderr

--- src/run_diff/run_diff_procedure.rs
+++ src/run_diff/run_diff_procedure.rs
@@ -2,7 +2,7 @@ use crate::configs::Configs;
 use crate::errors::BackupError;
 
 use std::io;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 fn get_ssh_dest_hot(configs: &Configs) -> String {
     format!(
@@ -12,19 +12,32 @@ fn get_ssh_dest_hot(configs: &Configs) -> String {
 }
 
 fn get_disk_usages(configs: &Configs) -> io::Result<()> {
-    let mut child_a = Command::new("du")
+    let child_a = Command::new("du")
         .args(["--summarize", "--bytes", &configs.source])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .spawn()?;
 
-    let mut child_b = Command::new("ssh")
+    let child_b = Command::new("ssh")
         .arg(get_ssh_dest_hot(&configs))
         .arg(format!("du --summarize --bytes {}", &configs.source))
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .spawn()?;
 
-    let status_a = child_a.wait()?;
-    let status_b = child_b.wait()?;
-    println!("{status_a}");
-    println!("{status_b}");
+    let status_a = child_a.wait_with_output()?;
+    let status_b = child_b.wait_with_output()?;
+
+    let stdout_a = String::from_utf8_lossy(&status_a.stdout);
+    let stderr_a = String::from_utf8_lossy(&status_a.stderr);
+
+    let stdout_b = String::from_utf8_lossy(&status_b.stdout);
+    let stderr_b = String::from_utf8_lossy(&status_b.stderr);
+
+    println!("{stdout_a}");
+    println!("{stderr_a}");
+    println!("{stdout_b}");
+    println!("{stderr_b}");
     Ok(())
 }
 
