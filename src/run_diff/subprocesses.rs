7c8c4ba0b93229b19f4a64a63d415cbbc9e181f4
dsw7 <dds3443@hotmail.com>
2026-07-27 23:33:51 -0700

    Pack results into vec of structs

--- src/run_diff/subprocesses.rs
+++ src/run_diff/subprocesses.rs
@@ -1,7 +1,7 @@
 use crate::configs::Configs;
 
 use std::io;
-use std::process::{Command, Stdio};
+use std::process::{Command, Output, Stdio};
 
 fn get_ssh_dest_hot(configs: &Configs) -> String {
     format!(
@@ -17,41 +17,58 @@ fn get_ssh_dest_cold(configs: &Configs) -> String {
     )
 }
 
-pub fn get_disk_usages(configs: &Configs) -> io::Result<()> {
-    let child_a = Command::new("du")
+pub struct Usage {
+    pub failed: bool,
+    pub host: String,
+    pub stderr: String,
+    pub stdout: String,
+}
+
+fn unpack_output(host: &String, output: &Output) -> Usage {
+    let stdout = if output.status.success() {
+        String::from_utf8_lossy(&output.stdout).into_owned()
+    } else {
+        String::from("-")
+    };
+
+    Usage {
+        failed: output.status.success(),
+        host: String::from(host),
+        stdout: stdout,
+        stderr: String::from_utf8_lossy(&output.stderr).into_owned(),
+    }
+}
+
+pub fn get_disk_usages(configs: &Configs) -> io::Result<Vec<Usage>> {
+    let proc_localhost = Command::new("du")
         .args(["--summarize", "--bytes", &configs.source])
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()?;
 
-    let child_b = Command::new("ssh")
+    let proc_hot_backup = Command::new("ssh")
         .arg(get_ssh_dest_hot(&configs))
         .arg(format!("du --summarize --bytes {}", &configs.source))
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()?;
 
-    let child_b = Command::new("ssh")
+    let proc_cold_backup = Command::new("ssh")
         .arg(get_ssh_dest_cold(&configs))
         .arg(format!("du --summarize --bytes {}", &configs.source))
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()?;
 
-    let output_a = child_a.wait_with_output()?;
-    let output_b = child_b.wait_with_output()?;
-    let output_c = child_c.wait_with_output()?;
+    let output_localhost = proc_localhost.wait_with_output()?;
+    let output_hot_backup = proc_hot_backup.wait_with_output()?;
+    let output_cold_backup = proc_cold_backup.wait_with_output()?;
 
-    let stdout_a = String::from_utf8_lossy(&output_a.stdout);
-    let stderr_a = String::from_utf8_lossy(&output_a.stderr);
-    let stderr_c = String::from_utf8_lossy(&output_c.stderr);
+    let usages = vec![
+        unpack_output(&String::from("localhost"), &output_localhost),
+        unpack_output(&configs.storage.hot.host, &output_hot_backup),
+        unpack_output(&configs.storage.cold.host, &output_cold_backup),
+    ];
 
-    let stdout_b = String::from_utf8_lossy(&output_b.stdout);
-    let stderr_b = String::from_utf8_lossy(&output_b.stderr);
-
-    println!("{stdout_a}");
-    println!("{stderr_a}");
-    println!("{stdout_b}");
-    println!("{stderr_b}");
-    Ok(())
+    Ok(usages)
 }
