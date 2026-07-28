372196a5cb091d41524f309c9ced7688b80c417c
dsw7 <dds3443@hotmail.com>
2026-07-27 22:21:28 -0700

    Get remote host usage

--- src/run_diff/run_diff_procedure.rs
+++ src/run_diff/run_diff_procedure.rs
@@ -5,12 +5,22 @@ use std::io;
 use std::process::Command;
 
 fn get_disk_usages(configs: &Configs) -> io::Result<()> {
-    let mut proc_local = Command::new("du")
+    let mut child_a = Command::new("du")
         .args(["--summarize", "--bytes", &configs.source])
         .spawn()?;
 
-    let status = proc_local.wait()?;
-    println!("{status}");
+    let mut child_b = Command::new("ssh")
+        .arg(format!(
+            "{}@{}",
+            &configs.storage.hot.user, &configs.storage.hot.host
+        ))
+        .arg(format!("du --summarize --bytes {}", &configs.source))
+        .spawn()?;
+
+    let status_a = child_a.wait()?;
+    let status_b = child_b.wait()?;
+    println!("{status_a}");
+    println!("{status_b}");
     Ok(())
 }
 
