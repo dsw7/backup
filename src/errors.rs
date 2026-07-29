dc466be30cf9a463a8db6b52b0fe402e18c1657b
dsw7 <dds3443@hotmail.com>
2026-07-29 04:49:38 -0700

    Apply clippy suggestion

--- src/errors.rs
+++ src/errors.rs
@@ -3,19 +3,19 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum BackupError {
     #[error("An I/O error occurred: {source}")]
-    IOError {
+    IO {
         #[from]
         source: std::io::Error,
     },
 
     #[error("An integer parsing error occurred: {source}")]
-    ParseIntError {
+    ParseInt {
         #[from]
         source: std::num::ParseIntError,
     },
 
     #[error("Failed to load TOML configurations: {source}")]
-    TOMLError {
+    ParseTOML {
         #[from]
         source: toml::de::Error,
     },
