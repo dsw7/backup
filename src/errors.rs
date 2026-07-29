93239459598277c549ce47d8f25fa0ebd0fba569
dsw7 <dds3443@hotmail.com>
2026-07-29 04:17:16 -0700

    Rename errors

--- src/errors.rs
+++ src/errors.rs
@@ -3,19 +3,19 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum BackupError {
     #[error("An I/O error occurred: {source}")]
-    Io {
+    IOError {
         #[from]
         source: std::io::Error,
     },
 
-    #[error("A parsing error occurred: {source}")]
-    ParseInt {
+    #[error("An integer parsing error occurred: {source}")]
+    ParseIntError {
         #[from]
         source: std::num::ParseIntError,
     },
 
-    #[error("Failed to load configurations: {source}")]
-    Configs {
+    #[error("Failed to load TOML configurations: {source}")]
+    TOMLError {
         #[from]
         source: toml::de::Error,
     },
