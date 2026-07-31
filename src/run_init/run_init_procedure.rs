67acb8699762cc5c6a4ef98ba8a5f5ab0bde06c8
dsw7 <dds3443@hotmail.com>
2026-07-31 02:45:13 -0700

    Add appdir generator

--- src/run_init/run_init_procedure.rs
+++ src/run_init/run_init_procedure.rs
@@ -1,5 +1,25 @@
 use crate::errors::BackupError;
+use crate::program_files;
+
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+
+fn create_app_dir() -> io::Result<PathBuf> {
+    let appdir = program_files::get_app_dir()?;
+
+    if !appdir.exists() {
+        fs::create_dir(&appdir)?;
+        println!(
+            "Created program application directory: {}",
+            appdir.display()
+        );
+    }
+
+    Ok(appdir)
+}
 
 pub fn initialize_program() -> Result<(), BackupError> {
+    let _ = create_app_dir()?;
     Ok(())
 }
